5941c4774573bfc0da8e4a40b121eaeeee67ab9b
Aman Raut <amanraut1111@gmail.com>
2024-02-16 12:13:11 +0530

    add  middleware

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { registerAdmin, adminLogin } = require('../controller/adminController');
+const auth = require('../middleware/auth');
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add', registerAdmin);
-adminRouter.post('/login', adminLogin)
+adminRouter.post('/login', auth, adminLogin)
 
 
 module.exports = adminRouter
