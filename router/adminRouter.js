4f9f6877638ff33125b97e12d077b06c2d448683
Aman Raut <amanraut1111@gmail.com>
2024-02-17 11:29:46 +0530

    make changes in controller

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { registerAdmin, adminLogin } = require('../controller/adminController');
-const auth = require('../middleware/auth');
+const isValidToken = require('../middleware/auth');
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add', registerAdmin);
-adminRouter.post('/login', auth, adminLogin)
+adminRouter.post('/login', isValidToken, adminLogin)
 
 
 module.exports = adminRouter
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { regUser, login } = require("../controller/userController");
+const isValidToken = require("../middleware/auth");
 
 const userRouter = express.Router();
 
 userRouter.post("/register", regUser);
-userRouter.post("/login", login);
+userRouter.post("/login", isValidToken, login);
 
 module.exports = userRouter;
