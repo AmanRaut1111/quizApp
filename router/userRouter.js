1b8e5e1d7d2a8ec671e87ef846d288b1ad18ba73
Aman Raut <amanraut1111@gmail.com>
2024-02-18 23:22:24 +0530

    add updatepassword function

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { regUser, login, forgotPassword } = require("../controller/userController");
+const { regUser, login, forgotPassword, updatePassword } = require("../controller/userController");
 const isValidToken = require("../middleware/auth");
 
 const userRouter = express.Router();
 
 userRouter.post("/register", regUser);
 userRouter.post("/login", isValidToken, login);
-userRouter.post('/forgotPassword/:id', forgotPassword)
+userRouter.post('/forgotPassword/:id', forgotPassword);
+userRouter.put('/updatePassword/:id', isValidToken, updatePassword)
 
 module.exports = userRouter;
