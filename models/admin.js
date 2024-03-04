a9de7fb924b601e241d509080dac49feabd6b54b
Aman Raut <amanraut1111@gmail.com>
2024-03-05 01:45:46 +0530

    remove one key

--- models/admin.js
+++ models/admin.js
@@ -1,27 +1,25 @@
 const mongoose = require("mongoose");
 
 const adminSchema = mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-    },
+
 
     phoneNo: {
         type: Number,
         required: true,
     },
 
-    password: {
+    otp: {
         type: String,
-        required: true,
     },
-
-
+    otp_generated_at: {
+        type: Date,
+        default: new Date(),
+    },
 
     upadtedOn: {
         type: Date,
-        default: new Date()
-    }
+        default: new Date(),
+    },
 });
 
 module.exports = mongoose.model("admin", adminSchema);
