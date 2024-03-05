14f68bc01395636cc8d28b7cfed0bb756fe1aa38
Aman Raut <amanraut1111@gmail.com>
2024-03-05 23:01:40 +0530

    add verifyotp and admin login function

--- controller/adminController.js
+++ controller/adminController.js
@@ -4,37 +4,28 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { otpGenerator, client } = require("../helpers/optpGenerator");
 const twilio = require("twilio");
+
 const registerAdmin = async (req, res) => {
     try {
-        const { phoneNo } = req.body;
-
-        const admindata = await adminModel({
+        const { userName, password, phoneNo } = req.body;
+        const adminData = await adminModel({
+            userName: userName,
+            password: await passwordhelper.hash(password),
             phoneNo: phoneNo,
         });
 
-        const otp = await otpGenerator();
+        const data = await adminData.save();
 
-        client.messages.create({
-            body: ` Your OTP is  ${otp}`,
-            from: process.env.from,
-            to: process.env.to,
-        });
-
-        const data = await admindata.save();
-
-        const token = jwt.sign({ _id: data._id }, process.env.SECRET_KEY, {
-            expiresIn: process.env.expiresIn,
-        });
         if (data) {
             res.status(200).json({
-                message: "OTP sent sucesssfully...!",
+                message: "Admin Registered sucessfully...!",
                 status: true,
-                statsuCode: 200,
-                token: token,
+                statusCode: 200,
+                data: data,
             });
         } else {
             res.status(400).json({
-                message: "Something Went Wrong...!",
+                message: "Something went Wrong...!",
                 status: false,
                 statusCode: 400,
             });
@@ -42,10 +33,81 @@ const registerAdmin = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            message: "Something Went Wrong...!",
+            message: "Something went Wrong...!",
             status: false,
             statusCode: 500,
         });
+    }
+};
+
+const sendOtp = async (req, res) => {
+    try {
+        const { phoneNo } = req.body;
+
+        const check = await adminModel.findOne({ phoneNo });
+        if (check) {
+            const otp = await otpGenerator();
+            client.messages.create({
+                body: ` Dear ${check.userName}  ${otp} is your one time password (OTP).Thank You,Team Nodejs`,
+                from: process.env.from,
+                to: process.env.to,
+            });
+
+            const data = await adminModel.updateOne({
+                otp: otp,
+                otp_generated_at: new Date(),
+            });
+            if (data) {
+                res.status(200).json({
+                    message: "OTP sent Sucessfully..!",
+                    status: true,
+                    statusCode: 200,
+                });
+            } else {
+                res.statu(400).json({
+                    message: "Something Went wrong...!",
+                    status: false,
+                    statusCode: 400,
+                });
+            }
+        } else {
+            res.status(401).json({
+                message:
+                    " Not Found...!, Please enter Your registered mobile number...!",
+                status: false,
+                statusCode: 401,
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something Went wrong..!",
+            status: false,
+            statusCode: 500,
+        });
+    }
+};
+
+const verifyOtp = async (req, res) => {
+    try {
+        const { phoneNo, otp } = req.body;
+
+        console.log(otp);
+
+        const check = await adminModel.findOne({ phoneNo: phoneNo });
+
+        console.log(check.otp);
+        if (!(check.otp === otp)) {
+            return res.status(401).json({ message: "invalid Otp" });
+        } else {
+            res.status(200).json({
+                message: "OTP verify sucessfully...!",
+                status: true,
+                statsuCode: 200,
+            });
+        }
+    } catch (error) {
+        console.log(error);
     }
 };
 
@@ -93,4 +155,6 @@ const adminLogin = async (req, res) => {
 module.exports = {
     registerAdmin,
     adminLogin,
+    verifyOtp,
+    sendOtp,
 };
