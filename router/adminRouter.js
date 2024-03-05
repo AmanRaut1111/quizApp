c552df6fb6fd1ff84f7ff5e28fe0caf0e6a13bc8
Aman Raut <amanraut1111@gmail.com>
2024-03-05 22:52:51 +0530

    add router of verifyOtp and admin login

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { registerAdmin, adminLogin } = require('../controller/adminController');
+const { registerAdmin, adminLogin, sendOtp, verifyOtp } = require('../controller/adminController');
 const isValidToken = require('../middleware/auth');
 
 const adminRouter = express.Router();
 
 adminRouter.post('/add', registerAdmin);
-adminRouter.post('/login', isValidToken, adminLogin)
+adminRouter.post('/login', isValidToken, adminLogin);
+adminRouter.post('/sendOtp', sendOtp)
+adminRouter.post('/verifyOtp', verifyOtp)
 
 
 module.exports = adminRouter
