a8a4d7ae96a0e82d34a86613442e021ba41f4915
Aman Raut <amanraut1111@gmail.com>
2024-03-05 23:00:57 +0530

    change  in line 34

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const db = require("./config/db");
-const twilio = require('twilio');
+
 require("dotenv").config();
 const questionRouter = require("./router/questionRouter");
 const userRouter = require("./router/userRouter");
 const answerRouter = require("./router/answerRouter");
 const adminRouter = require("./router/adminRouter");
 
-
-
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Quiz");
+  res.send("Welcome to Quiz");
 });
-
-
 
 app.use(express.json());
 
@@ -23,8 +19,8 @@ app.use("/", questionRouter);
 
 app.use("/user", userRouter);
 app.use("/answer", answerRouter);
-app.use('/admin', adminRouter)
+app.use("/admin", adminRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log("server is listening on port 5000");
+  console.log("server is listening on port 5000");
 });
