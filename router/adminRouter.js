a38253c8d90b48065454c9cc18c1f55471da9f13
Aman Raut <amanraut1111@gmail.com>
2024-03-06 12:00:00 +0530

    add router for updatepassword

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerAdmin, adminLogin, sendOtp, verifyOtp } = require('../controller/adminController');
+const { registerAdmin, adminLogin, sendOtp, verifyOtp, updatePassword } = require('../controller/adminController');
 const isValidToken = require('../middleware/auth');
 
 const adminRouter = express.Router();
@@ -7,7 +7,8 @@ const adminRouter = express.Router();
 adminRouter.post('/add', registerAdmin);
 adminRouter.post('/login', isValidToken, adminLogin);
 adminRouter.post('/sendOtp', sendOtp)
-adminRouter.post('/verifyOtp', verifyOtp)
+adminRouter.post('/verifyOtp', verifyOtp);
+adminRouter.post('/updatePassword/:_id', updatePassword)
 
 
 module.exports = adminRouter
